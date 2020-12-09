1bf16fd5f6d9ba9cefe02aa8dfbca923a386c0c9
Emmanuel Zelarayan <emmavz@live.com>
2020-12-09 11:54:19 -0300

    add animated Scroll up

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@
 $('.carousel').carousel({
   interval: 5000
 })
+
+//Animate Scroll
+let animate = document.querySelectorAll(".animated-scroll-up");
+function seeScroll() {
+  let scrollTop = document.documentElement.scrollTop;
+  for (let i = 0; i < animate.length; i++) {
+    let heightAnimate = animate[i].offsetTop;
+    if (heightAnimate - 500 < scrollTop) {
+      animate[i].style.opacity = 1;
+      animate[i].classList.add("view-from-above");
+    }
+  }
+}
+window.addEventListener('scroll', seeScroll);
