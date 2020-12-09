8098dff87064abde74c0269178820abc239a3d7a
Emmanuel Zelarayan <emmavz@live.com>
2020-12-09 12:33:33 -0300

    add animate scroll right

--- js/script.js
+++ js/script.js
@@ -3,16 +3,30 @@ $('.carousel').carousel({
   interval: 5000
 })
 
-//Animate Scroll
-let animate = document.querySelectorAll(".animated-scroll-up");
-function seeScroll() {
+//Animate Scroll up
+let animateY = document.querySelectorAll(".animated-scroll-up");
+function seeScrollY() {
   let scrollTop = document.documentElement.scrollTop;
-  for (let i = 0; i < animate.length; i++) {
-    let heightAnimate = animate[i].offsetTop;
+  for (let i = 0; i < animateY.length; i++) {
+    let heightAnimate = animateY[i].offsetTop;
     if (heightAnimate - 500 < scrollTop) {
-      animate[i].style.opacity = 1;
-      animate[i].classList.add("view-from-above");
+      animateY[i].style.opacity = 1;
+      animateY[i].classList.add("view-from-above");
     }
   }
 }
-window.addEventListener('scroll', seeScroll);
+window.addEventListener('scroll', seeScrollY);
+
+// Animate Scroll right
+let animateX = document.querySelectorAll(".animated-scroll-right");
+function seeScrollX() {
+  let scrollRight = document.documentElement.scrollTop;
+  for (let i = 0; i < animateX.length; i++) {
+    let rightAnimate = animateX[i].offsetTop;
+    if (rightAnimate - 500 < scrollRight) {
+      animateX[i].style.opacity = 1;
+      animateX[i].classList.add("view-from-right");
+    }
+  }
+}
+window.addEventListener('scroll', seeScrollX);
