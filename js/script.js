3083f5a2a6f62ffdf55ab75c19b9c8cb11882695
Emmanuel Zelarayan <emmavz@live.com>
2020-12-09 19:52:47 -0300

    ...

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function seeScrollY() {
   let scrollTop = document.documentElement.scrollTop;
   for (let i = 0; i < animateY.length; i++) {
     let heightAnimate = animateY[i].offsetTop;
-    if (heightAnimate - 550 < scrollTop) {
+    if (heightAnimate - 500 < scrollTop) {
       animateY[i].style.opacity = 1;
       animateY[i].classList.add("view-from-above");
     }
@@ -23,7 +23,7 @@ function seeScrollX() {
   let scrollRight = document.documentElement.scrollTop;
   for (let i = 0; i < animateX.length; i++) {
     let rightAnimate = animateX[i].offsetTop;
-    if (rightAnimate - 550 < scrollRight) {
+    if (rightAnimate - 500 < scrollRight) {
       animateX[i].style.opacity = 1;
       animateX[i].classList.add("view-from-right");
     }
